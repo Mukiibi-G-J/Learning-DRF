5bb272dc0c22bab97626632d63b9d090af9f4ce0
unknown <m-g-j@m-g-jose.lubagahospital.org>
2023-08-16 07:03:51 +0300

    Algolia Search Client for DJango

---
+++ backend/search/client.py
@@ -0,0 +1,33 @@
+from algoliasearch_django import algolia_engine
+
+
+def get_client():
+    return algolia_engine.client
+
+
+def get_index(index_name="jeff_Product"):
+    client = get_client()
+
+    index = client.init_index(index_name)
+
+    return index
+
+
+# perform_search("hello", tags=["electronics"], public=True)
+
+
+def perform_search(query, **kwargs):
+    index = get_index()
+    tags = ""
+    params = {}
+    if "tags" in kwargs:
+        tags = kwargs.pop("tags") or []
+
+        if len(tags) != 0:
+            params["tagFilters"] = tags
+    index_filters = [f"{k}:{v}" for k, v in kwargs.items() if v]
+    if len(index_filters) != 0:
+        params["facetFilters"] = index_filters
+    results = index.search(query, params)
+
+    return results
--- backend/search/views.py
+++ backend/search/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from rest_framework import generics
 from products.models import Product
 from products.serializers import ProductSerializer
+from rest_framework.response import Response
+from . import client
 
 
-class SearchListView(generics.ListAPIView):
+class SearchListView(generics.GenericAPIView):
+    def get(self, *args, **kwargs):
+        user = None
+        if self.request.user.is_authenticated:
+            user = self.request.user.username
+
+        query = self.request.GET.get("q")
+        tag = self.request.GET.get("tag") or None
+        public = str(self.request.GET.get("public")) != "0"
+
+        if not query:
+            return Response("", status=400)
+        print(user, query, public, tag)
+        results = client.perform_search(query, tags=tag, user=user, public=public)
+        return Response(results)
+
+
+class SearchOldListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
